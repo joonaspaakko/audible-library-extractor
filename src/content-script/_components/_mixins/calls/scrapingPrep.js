92485719061f450607ec6b95b1b7ec0c63a26929
Joonas Pääkkö <paakkoj@gmail.com>
2026-06-06 01:10:04 +0300

    Related to #148 → Trying to simplify this function to eliminate possible errors...

--- src/content-script/_components/_mixins/calls/scrapingPrep.js
+++ src/content-script/_components/_mixins/calls/scrapingPrep.js
@@ -1,9 +1,7 @@
-// This basically probes the target page for what its maximum page size is...
-// Then it probes the page again with that page size...
-// ...and returns the amount of pages on that
-
-// Then you're ready to start scraping the pages at the same time.
+// Fetches the target page with the preferred page size, reads pagination,
+// and returns all page numbers. Ready to scrape all pages in parallel.
 import rateLimit from "axios-rate-limit";
+import axiosRetry from "axios-retry";
 
 export default {
   methods: {
@@ -16,101 +14,64 @@ export default {
       let url = new Url(DOMPurify.sanitize(config.url));
       
       const urlAlreadyFailed = _.includes(vue.$store.state.failedRequests, vue.getRequestId( url.toString() ));
-      if (urlAlreadyFailed) {
+      if ( urlAlreadyFailed ) {
         config.done({});
         return;
       }
       
       const letMeAxiosAQuestion = axios.create();
-      // axiosRetry(letMeAxiosAQuestion, {
-      //   retries: 2,
-      //   retryDelay: function(retryCount) { return 1000 * retryCount; },
-      //   retryCondition: function(error) {
-      //     return axiosRetry.isNetworkOrIdempotentRequestError(error) || error && error.response && error.response.status == "500";
-      //   }
-      // });
-      // const axiosLimited = rateLimit(letMeAxiosAQuestion, { maxRPS: 15 });
-      
+      axiosRetry(letMeAxiosAQuestion, {
+        retries: 2,
+        retryDelay: retryCount => retryCount * 1000,
+        retryCondition: e => e.response?.status === 503,
+      });
+
       const limiter = _.cloneDeep(this.$store.state.axiosRateLimit);
       limiter.maxRequests = _.clamp( Math.ceil(limiter.maxRequests * .5), 1, limiter.maxRequests );
       const axiosLimited = rateLimit(letMeAxiosAQuestion, limiter);
-      
-      waterfall(
-        [
-          // GET MAX PAGE SIZE (how many items per page)
-          function(callback) {
-            url.query.ale = true;
-            url.query.bp_ua = 'yes';
-            let obj = {};
-            obj.urlObj = url;
-            
-            let result = [ null, {}];
-            
-            if ( config.skipFirstCall ) {
-              result = vue.getMaxPageSize( obj, config, config.response, callback);
-              callback( result[0], result[1] );
-            }
-            else {
-              
-              
-              axiosLimited.get( url.toString() )
-              .then(function(response) {
-                result = vue.getMaxPageSize( obj, config, response, callback);
-              })
-              .catch(function( e ) {
-                const status = _.get(e, 'response.status');
-                if (status == 404) vue.$store.commit('pushToFailedRequests', vue.getRequestId( url.toString() ));
-                result = [ true, {} ];
-              })
-              .then(function() {
-                
-                callback( result[0], result[1] );
-                
-              });
-              
-            }
-            
-          },
-          
-          // GET MAX PAGE COUNT (pagination)
-          function(o, callback) {
-            axiosLimited.get(o.urlObj.toString()).then(function(response) {
-              const audible = $($.parseHTML(response.data)).find("div.adbl-main");
-              const pagination = audible.find(".pagingElements");
-              const pagesLength = pagination.length > 0 ? parseFloat( DOMPurify.sanitize(pagination.find(".pageNumberElement:last").text()) ) : 1;
-              o.pageNumbers = _.range(1, pagesLength + 1);
-              o.pageSize = o.urlObj.query.pageSize || null;
-              callback(null, o);
-            });
-          }
-        ],
-        function(err, obj) {
-          config.done(obj);
+
+      url.query.ale = true;
+      url.query.bp_ua = 'yes';
+      url.query.pageSize = config.maxSize || url.query.pageSize || 50;
+
+      let obj = { urlObj: url };
+
+      const processResponse = function( response ) {
+        const data = _.get(response, 'data');
+        if ( !data ) {
+          config.done({});
+          return;
         }
-      );
-    },
-    
-    getMaxPageSize: function( obj, config, response, waterfallback ) {
-      
-      const data = _.get(response, 'data');
-      if ( !data ) return [ null, null ];
-      
-      const audible = $($.parseHTML(data)).find("div.adbl-main");
-      const pageSizeDropdown = audible.find('select[name="pageSize"]');
-      const maxPageSize = pageSizeDropdown.length > 0 ? DOMPurify.sanitize(pageSizeDropdown.find("option:last").val()) : null;
-      obj.urlObj.query.pageSize = config.maxSize || obj.urlObj.query.pageSize || maxPageSize;
-      
-      if ( config.returnResponse ) obj.response = response;
-      
-      const pagination = audible.find(".pagingElements").length;
-      if ( !pagination || !maxPageSize || maxPageSize < 50 || config.returnAfterFirstCall ) {
-        obj.pageNumbers = [1];
+
+        const audible = $($.parseHTML(data)).find("div.adbl-main");
         obj.pageSize = obj.urlObj.query.pageSize;
-        return [ true, obj ];
-      } else {
-        return [ null, obj ];
+
+        if ( config.returnResponse ) obj.response = response;
+
+        const pagination = audible.find(".pagingElements");
+        const pagesLength = ( pagination.length > 0 && !config.returnAfterFirstCall )
+          ? parseFloat( DOMPurify.sanitize(pagination.find(".pageNumberElement:last").text()) )
+          : 1;
+        obj.pageNumbers = _.range(1, pagesLength + 1);
+
+        config.done(obj);
+      };
+
+      if ( config.skipFirstCall ) {
+        processResponse(config.response);
       }
-            
+      else {
+        axiosLimited.get( url.toString() )
+        .then(function( response ) {
+          processResponse(response);
+        })
+        .catch(function( e ) {
+          const status = _.get(e, 'response.status');
+          if (status == 404) vue.$store.commit('pushToFailedRequests', vue.getRequestId( url.toString() ));
+          config.done({});
+        });
+      }
+
     },
     
     getPageNumbers: function( response  ) {
