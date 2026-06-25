joonaspaakko/audible-library-extractor

commit 945de1b648874204efbad4e1f92e7555f22b8aef

Switched Lodash imports to the Node package.
- Not sure why I even had a cdn import for lodash when there was a /src/assets/js/lodash.min.js right there waiting to be used... Probably artifacts of the old Webpack to Vite switch...

vite.config.js
@@ -46,6 +46,9 @@ const copyFilesBefore = [
 const copyFilesAfter = [
   { src: src('extension-js'),            dest: 'assets' },
   { src: src('assets/js'),               dest: 'assets' },
+  // Lodash is used as a bare global `_` across the app. Copy it from node_modules
+  // so it can load as a classic script before any module chunk runs.
+  { src: root('node_modules/lodash/lodash.min.js'), dest: 'assets/js' },
   { src: src('gallery/app.webmanifest'), dest: dist('') },
   { src: src('gallery/images'),          dest: dist('assets') },
   { src: src('gallery/favicons'),        dest: dist('') },

gallery.html
@@ -6,8 +6,11 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>My Audible Library</title>
   
+  <!-- Lodash is used as a bare global `_` across the app. Load it as a classic -->
+  <!-- script so it runs before any module chunk evaluates a component. -->
+  <script src="assets/js/lodash.min.js"></script>
+
   <% if ( !singleFileMode ){ %>
-    <script defer src="assets/js/lodash.min.js"></script>
     <script defer src="assets/extension-js/papaparse.min.js"></script> 
     <script defer src="file-paths.js"></script>
   <% } %>

src/gallery/_components/snippets/save-gallery.vue
@@ -577,12 +577,12 @@ export default {
             <title>My Audible Library</title>
             <script type="module" src="https://esm.sh/@khmyznikov/pwa-install">${sc}
             ${loadServiceWorker}
+            <script src="assets/js/lodash.min.js">${sc}
             <link id="ale-css" rel="stylesheet" href="${ getFile('gallery', 'css') }">
           </head>
           <body>
             <div id="audible-library-extractor" data-version="${ this.$store.state.version }" data-cache-id="${ vue.cacheBuster }"></div>
             <pwa-install use-local-storage manifest-url="app.webmanifest" icon="favicons/android-chrome-192x192.png" name="My Audible Library"></pwa-install>
-            <script src="https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js">${sc}
             <script id="ale-js" src="${ getFile('gallery', 'js') }" type="module">${sc}
             <noscript>This library requires javascript to work!</noscript>
           </body>
@@ -635,6 +635,7 @@ export default {
         let assetFiles = _.cloneDeep(vue.files);
         
         assetFiles = assetFiles.concat([
+          "assets/js/lodash.min.js",
           "favicons/android-chrome-192x192.png",
           "favicons/android-chrome-512x512.png",
           "favicons/apple-touch-icon.png",

wallpaper-creator.html
@@ -5,7 +5,10 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>ALE • Wallpaper editor</title>
-  <script defer src="assets/js/lodash.min.js"></script>
+
+  <!-- Lodash is used as a bare global `_` across the app. Load it as a classic -->
+  <!-- script so it runs before any module chunk evaluates a component. -->
+  <script src="assets/js/lodash.min.js"></script>
 </head>
 <body>
   

commit 9ecdf09563b134c73ffdafe8b9153a51da15b739

Fixed a build issue introduced in 945de1b648874204efbad4e1f92e7555f22b8aef

vite.config.js
@@ -45,7 +45,7 @@ const copyFilesBefore = [
 ];
 const copyFilesAfter = [
   { src: src('extension-js'),            dest: 'assets' },
-  { src: src('assets/js'),               dest: 'assets' },
+  // { src: src('assets/js'),               dest: 'assets' },
   // Lodash is used as a bare global `_` across the app. Copy it from node_modules
   // so it can load as a classic script before any module chunk runs.
   { src: root('node_modules/lodash/lodash.min.js'), dest: 'assets/js' },