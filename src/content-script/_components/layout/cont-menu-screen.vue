75f4af093802231a0aef7b35514fcdd38b692562
Joonas Pääkkö <paakkoj@gmail.com>
2026-06-30 15:25:16 +0300

    Trying to declutter the extraction settings view.

--- src/content-script/_components/layout/cont-menu-screen.vue
+++ src/content-script/_components/layout/cont-menu-screen.vue
@@ -1,135 +1,56 @@
 <template>
+<n-config-provider :theme="lightTheme" :theme-overrides="themeOverrides">
 <div>
-  <div id="ale-menu-screen" v-if="!loading" :class="{ 'show-delete-btns': showDeleteBtns, 'show-partial-extraction': showPartialExtraction }">
-    
+  <div id="ale-menu-screen" v-if="!loading">
+
     <!-- SETTINGS -->
-    <cont-extraction-settings />
-  
-    <!-- INLINE MESSAGES -->
-    <!-- <article v-if="dataVersionMismatch.length > 0" class="message is-danger">
-      <div class="message-body">
-        You should <span class="init-show-detele-btns" @mouseover="showDeleteBtns = true" @mouseleave="showDeleteBtns = false">remove</span> <strong>{{ dataVersionMismatch.join(', ') }}</strong> data and re-extract to get the most out of the new version.
-      </div>
-    </article> -->
-    
-    <article v-if="$store.getters.partialDataSettings_any" class="message is-info">
-      <div class="message-body">
-        <strong>Previously extracted data detected: <span style="color: #f7991c;">a faster <span v-tippy="{ allowHTML: true }" content="<div style='display: block; text-align: left;'><ol style='margin-left: 15px;'><li>Updates stored data that is likely to change</li><li>Does a full extract on newly added books</li><li>Clears data of removed books</li></ol><span style='display: inline-block; margin-top: 6px; color: #157df0;'>In the case of ISBN extraction, books that already have ISBNs are ignored. A large amount of books (+100) without the numbers will still take quite a while to extract.</span></div>" style="cursor: default; text-decoration: underline">partial extraction</span> is used</span> <span class="init-show-partial-extraction" style="color: #f7991c;" @mouseover="showPartialExtraction = true" @mouseleave="showPartialExtraction = false">where applicable.</span></strong> 
-        <br>
-        If you need to do a <span v-tippy="{ maxWidth: 310 }" content="The data structure may change from version to version, in which case a full extraction is needed or at least preferred. <br><br>You need to remove extracted data in order to do a full extraction." style="cursor: default; text-decoration: underline">full extraction</span>, you can remove stored data using <span class="init-show-detele-btns" @mouseover="showDeleteBtns = true" @mouseleave="showDeleteBtns = false">these buttons</span>.
-      </div>
-    </article>
-    
-    <!-- EXTRACTION BUTTONS -->
-    <div>
-      <div class="extract-wrapper">
-        
-        <div class="field has-addons extract-btn" v-show="!store.extractionButtonDisabled && !loading">
-          <p class="control">
-            <button class="button is-info extract is-large" @click="extract" expanded>
-              Extract selected items
-            </button>
-          </p>
-          <p class="control">
-            <button class="button is-dark is-large" @click="extract">
-              <ph-arrow-circle-right-bold/>
-            </button>
-          </p>
-        </div>
+    <cont-extraction-settings @extract="extract" @open-gallery="takeNextStep('output')" />
 
-        <div class="field has-addons other-btns">
-          <a
-            class="button control is-small"
-            href="https://joonaspaakko.gitbook.io/audible-library-extractor/"
-            target="_blank"
-          >
-            Documentation <ri-external-link-line/>
-          </a>
-          <button
-            class="button control is-small"
-            :disabled="!$store.getters.mainDataExists ? true : null"
-            @click="takeNextStep('output')"
-          >
-            Open gallery <ri-external-link-line/>
-          </button>
-        </div>
-      </div>
-    </div>
-
-    <cont-changelog></cont-changelog>
-
-
-    <div id="footer" class="is-small has-text-grey-light">
-      Project source files in <a href="https://github.com/joonaspaakko/audible-library-extractor">Github</a>. <br />
-      Post issues, questions, and suggestions at: <a href="https://github.com/joonaspaakko/audible-library-extractor/issues">Github issues</a>. <br>
-      <br>
-      
-      <!-- <b-icon style="position: relative; top: -4px;"
-        v-tippy content="It may take a few days for a new release to propagate from Github to Firefox and Chrome web store."
-        icon-pack="fas"
-        icon="question-circle"
-        size="is-small">
-      </b-icon> -->
-      
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor/releases/latest" v-tippy content="It may take a few days for a new release to propagate from Github to Firefox and Chrome web store.">
-        <img src="https://img.shields.io/github/v/release/joonaspaakko/audible-library-extractor?include_prereleases&label=latest%20release (Github)&color=6e41bf" alt="">
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor/releases/latest">
-        <img src="https://img.shields.io/github/release-date/joonaspaakko/audible-library-extractor?label=latest%20release&color=6e41bf" alt="">
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor/labels/bug">
-        <img src="https://img.shields.io/github/issues/joonaspaakko/audible-library-extractor/bug?label=known bugs&color=6e41bf" alt="">
-      </a>
-
-      <br>
-      
-      <a target="_blank" rel="noopener noreferrer" href="https://chrome.google.com/webstore/detail/audible-library-extractor/deifcolkciolkllaikijldnjeloeaall">
-        <img src="https://img.shields.io/chrome-web-store/v/deifcolkciolkllaikijldnjeloeaall?color=2acb41&label=latest%20release (Chrome)" alt="">
-      </a>
-      <a target="_blank" rel="noopener noreferrer" href="https://addons.mozilla.org/en-US/firefox/addon/audible-library-extractor/">
-        <img src="https://img.shields.io/amo/v/audible-library-extractor?label=latest%20release (Firefox)" alt="">
-      </a>
-      
-      <div style="margin-top: 10px;">
-        <span style="border: 1px dashed #e1e1e1; padding: 10px 15px; display: inline-block;">
-          You're currently using <strong>version {{ store.appVersion }}</strong>.
-        </span>
-      </div>
-    </div>
   </div>
   <div v-else class="extraction-loading">
     <gg-spinner class="spin" />
   </div>
-  
+
 </div>
+</n-config-provider>
 </template>
 
 <script>
+import { NConfigProvider, lightTheme } from 'naive-ui';
 import helpers from "@contscript-mixins/misc/content-script-helpers.js";
 
 export default {
   name: "menuScreen",
   props: ["domainExtension", "wishlistUrl"],
   mixins: [ helpers ],
+  components: { NConfigProvider },
   data() {
     return {
       store: this.$store.state,
       hasData: null,
       hasConfig: null,
-      settingsOpen: true,
       loading: true,
       isFirefox: false,
       isChrome: false,
       releaseURL: '',
-      showDeleteBtns: false,
-      showPartialExtraction: false,
-      exportRawDataDisabled: false,
       cannotAccessWishlist: false,
     };
   },
   
   computed: {
-    
+
+    lightTheme: function() { return lightTheme; },
+
+    themeOverrides: function() {
+      return {
+        common: {
+          primaryColor: '#f79a1c',
+          primaryColorHover: '#f8a93d',
+          primaryColorPressed: '#dd860e',
+        },
+      };
+    },
+
     dataVersionMismatch: function() {
       
       // const extensionVersion = this.store.appVersion;
@@ -266,23 +187,28 @@ export default {
 };
 </script>
 
-<style scoped src="@node/bulma/css/bulma.css"></style>
-
 <style lang="scss">
-// @use '@node/bulma/css/bulma.css';
 
 body > .notices {
   z-index: 9999999999 !important;
 }
 
+// naive-ui's floating layers (tree-select/dropdown/popover popups, popconfirm, the
+// discrete message container) teleport to <body>, landing as siblings of the
+// #audible-library-extractor overlay rather than inside it. Without this they default
+// to naive-ui's own z-index (~2000), which sits behind the overlay's z-index:999999999
+// and makes every popup open invisibly/unclickably. Match tippy's existing fix for the
+// same problem.
+.v-binder-follower-container,
+.n-message-container,
+.n-modal-container {
+  z-index: 9999999990 !important;
+}
+
 #ale-menu-screen {
   max-width: 810px;
   margin: 0 auto;
-  
-  > .panel {
-    box-shadow: none !important;
-  }
-  
+
   /* ----------------------------------------------
   * Generated by Animista on 2020-12-19 21:31:49
   * Licensed under FreeBSD License.
@@ -311,300 +237,6 @@ body > .notices {
     }
   }
 
-  .has-text-grey-light {
-    a {
-      text-decoration: underline;
-      color: #b5b5b5;
-    }
-    a:visited {
-      color: color.adjust(#b5b5b5, $lightness: -10%);
-    }
-  }
-
-  .extract-wrapper {
-    display: inline-block;
-    margin: 20px auto 0;
-    .field-body { width: 100%; }
-  }
-
-  .extract-btn {
-    margin-bottom: 0px !important;
-    display: inline-flex;
-    width: 100%;
-    button.extract {
-      padding: 5px 15px 4px !important;
-      border-radius: 4px 0 0 4px;
-      background: #1b7dd1 !important;
-    }
-    button.is-dark {
-      background: #555 !important;
-    }
-  }
-
-  .extract-settings {
-    position: relative;
-    z-index: 1;
-    padding-top: 10px;
-    margin: 0px auto 20px auto;
-    border: 1px solid #e1e1e1;
-    border-radius: 6px;
-
-    span.check {
-      top: -1px;
-      position: relative;
-    }
-
-    .title {
-      &.is-4 {
-        margin-bottom: 2px;
-      }
-      &.is-6 {
-        margin-bottom: 7px;
-        color: #666;
-      }
-    }
-    .field {
-      justify-content: center;
-    }
-    .description {
-      line-height: 22px;
-      margin: 0;
-      .message-body {
-        // padding: 13px;
-        padding: 5px;
-        font-size: .9em;
-        line-height: 1.2em;
-        border: none !important;
-      }
-      &.darker-gray {
-        border-radius: 0;
-        background: rgba(#000, 0.1);
-        border-color: rgba(#000, 0.12);
-        border-style: solid;
-        border-width: 2px;
-        border-right: none;
-        border-left: none;
-        .media-content {
-          color: rgba(#000, 0.57);
-        }
-      }
-      .tag {
-        background-color: #d5e9f8 !important;
-        color: #1d72aa !important;
-      }
-    }
-
-    .settings-heading {
-      z-index: 2;
-      position: absolute;
-      left: 0;
-      right: 0;
-      top: -13px;
-      font-size: 1.3em;
-      span {
-        display: inline-block;
-        padding: 0 5px;
-        background: #fff;
-      }
-    }
-
-    .setting-checkboxes {
-      display: block;
-      // padding: 20px 6px;
-      padding: 15px 6px;
-      > div {
-        display: inline-block;
-			  padding-left: 11px;
-        &:first-child { padding-left: 0; }
-      }
-      @media ( max-width: 798px ) {
-        
-        padding: 20px;
-        > div {
-			    padding-left: 0px;
-          display: block;
-        }
-      }
-      &, .field {
-        margin: 0 !important;
-      }
-      span > .field {
-        padding: 3px;
-      }
-      // margin-right: 46px;
-      button {
-        padding: 8px;
-        min-width: 34px;
-        font-size: 13.5px;
-        line-height: 13.5px;
-        // .b-checkbox { margin-top: 2px; }
-        
-        
-      }
-      button span {
-        display: flex;
-        justify-content: center;
-        align-content: center;
-        justify-items: center;
-        align-content: center;
-      }
-      .checkbox-btn {
-        padding: 0;
-        .b-checkbox { 
-          padding: 10px 12px 9px;
-          margin: 0; 
-          .control-label { margin-top: -1px; }
-        }
-        &:disabled {
-          opacity: 1;
-          border: 1px solid #ededed;
-        }
-      }
-      
-      .full-width {
-        flex: 1;
-        > .button {
-          width: 100%;
-          display: flex;
-          justify-content: stretch;
-          align-items: stretch;
-          > label {
-            width: 100%;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-          }
-        }
-      }
-      
-      button {
-        outline: none !important;
-        box-shadow: none !important;
-        border-color: #dbdbdb !important;
-        &:active { border-color: #4a4a4a !important; }
-      }
-      
-      .setting-numbers-wrapper,
-      .checbox-wrapper {
-        // cursor: default;
-        // border: 1px solid #dbdbdb !important;
-        padding: 6px;
-        > .button {
-          border: none !important;
-          outline: none !important;
-          box-shadow: none !important;
-          font-size: 0.7em;
-          cursor: default;
-          padding: 0px;
-          width: 19px;
-          height: 19px;
-          border-radius: 4px;
-        }
-      }
-      
-      .disabled .checbox-wrapper > .button {
-        background: #000;
-      }
-      
-      & > .unchecked .checbox-wrapper > .button {
-        padding: 0;
-        font-size: 1em;
-      }
-      
-      .setting-numbers-wrapper > .button {
-        font-size: 1em !important;
-        padding: 0 !important;
-      }
-      
-    }
-  }
-
-  .other-btns {
-    margin: 6px auto 0;
-    a,
-    button {
-      width: 100%;
-      box-sizing: border-box;
-      height: auto;
-      padding-top: 9px;
-      padding-bottom: 9px;
-      flex-grow: 1;
-      font-size: 13px;
-      line-height: 13px;
-    }
-    svg { 
-      margin-left: 6px; 
-      font-size: 15px;
-      line-height: 15px;
-      position: relative;
-      top: -2px;
-    }
-  }
-
-  #footer {
-    font-size: 0.9em;
-    line-height: 1.3em;
-    margin-top: 30px;
-    padding-top: 20px;
-    padding-bottom: 60px;
-    a, img { display: inline-block; }
-    a {
-      margin: 2px;
-    }
-    &, * {
-      transition: all 200ms ease;
-      color: rgba(181, 181, 181, .5) !important;
-    }
-    img { opacity: .2; }
-    &:hover {
-      &, * {
-        color: #717171 !important;
-      }
-      a {
-        color: color.adjust(#717171, $lightness: -5%) !important;
-      }
-      img { opacity: 1; }
-    }
-  }
-
-  .settings-btn {
-    margin-top: 25px;
-    transition: all 180ms ease-out;
-    &:focus {
-      box-shadow: none !important;
-    }
-    &.open {
-      background: #444;
-      color: #fff;
-      border-radius: 5px;
-    }
-  }
-  
-  .linky-links {
-    margin-top: 10px;
-    background: #f4f5f6;
-    font-size: 13px;
-    span { 
-      margin-left: 6px; &:first-child { margin-left: 0 }
-    }
-    .divider { color: #444; }
-    a { color: #666; }
-    button {
-      margin: 5px 2px;
-      label { cursor: pointer; }
-      span { font-size: 11px !important; line-height: 11px !important; }
-      height: auto !important;
-    }
-  }
-  
-  .remove-individual-sections-icon {
-    // color: #959595;
-    color: #666;
-    svg {
-      width: 14px;
-    }
-  } 
-  
 }
 
 .update-tooltip {
@@ -640,38 +272,6 @@ body > .notices {
   color: red;
 }
 
-.show-partial-extraction .partial-extraction,
-.show-delete-btns .delete-btn {
-  position: relative;
-  z-index: 0;
-  &:before {
-    content: '';
-    position: absolute;
-    z-index: 90;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: rgba(red, .1);
-    outline: 1px solid rgba(red, .4);
-  }
-}
-
-.init-show-partial-extraction,
-.init-show-detele-btns {
-  cursor: default;
-  text-decoration: underline;
-  // border: 1px solid rgba(#956d03, .2);
-  // border-radius: 3px;
-  // padding: 0px 3px;
-}
-
-.show-partial-extraction .partial-extraction:before {
-  background: rgba(#47c78e, .25);
-  outline: 2px solid rgba(#47c78e, .8);
-}
-
-
 .tippy-box {
   box-shadow: 0 10px 25px rgba(#000, .2) !important;
 }
@@ -685,10 +285,6 @@ body > .notices {
   color: #151515 !important;
 }
 
-
-div.c-toast-container {
-  z-index: 9999999999 !important;
-}
 
 .no-selection {
   -webkit-touch-callout: none;
--- src/content-script/_components/layout/cont-menu-screen/cont-changelog.vue
+++ src/content-script/_components/layout/cont-menu-screen/cont-changelog.vue
@@ -1,74 +1,77 @@
 <template>
-<div id="changelog">
- 
-  <button
-    class="button is-small is-text settings-btn"
-    v-tippy="{ 
-      allowHTML: true, 
-      trigger: 'click',
-      interactive: true,
-    }"
-    :content="getChangelog()"
-  >
-    <ph-code-bold class="icon" />
-    <span>Changelog</span>
-  </button>
-  
-</div>
+<n-config-provider :theme="lightTheme">
+<n-modal
+  :show="show"
+  preset="card"
+  title="What's new"
+  style="width: 480px; max-width: 90vw;"
+  :bordered="false"
+  size="medium"
+  @update:show="$emit('update:show', $event)"
+>
+  <n-timeline class="changelog-body">
+    <n-timeline-item
+      v-for="versionBlock in changeLog"
+      :key="versionBlock.version"
+      :title="versionBlock.version"
+      type="info"
+    >
+      <div v-if="versionBlock.highlights" class="version-highlights">{{ versionBlock.highlights }}</div>
+      <ul class="ale-changelog-list">
+        <template v-for="(change, index) in versionBlock.changes" :key="index">
+          <li v-if="change.divider" class="changelog-divider"></li>
+          <li v-else :class="change.class">
+            <a v-if="change.link" target="_blank" rel="noopener noreferrer" :href="change.link.href">
+              <strong v-if="change.highlight">{{ change.link.text }}</strong>
+              <template v-else>{{ change.link.text }}</template>
+            </a><template v-if="change.link">: </template>{{ change.description }}
+          </li>
+        </template>
+      </ul>
+    </n-timeline-item>
+  </n-timeline>
+
+  <div class="project-info">
+    <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor">Source</a>
+    <span class="divider">·</span>
+    <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor/issues">Report an issue</a>
+    <span class="divider">·</span>
+    <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor/labels/bug">Known bugs</a>
+
+    <div class="project-badges">
+      <a target="_blank" rel="noopener noreferrer" href="https://github.com/joonaspaakko/audible-library-extractor/releases/latest">
+        <img src="https://img.shields.io/github/v/release/joonaspaakko/audible-library-extractor?include_prereleases&label=latest%20release%20(Github)&color=6e41bf" alt="">
+      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://chrome.google.com/webstore/detail/audible-library-extractor/deifcolkciolkllaikijldnjeloeaall">
+        <img src="https://img.shields.io/chrome-web-store/v/deifcolkciolkllaikijldnjeloeaall?color=2acb41&label=latest%20release%20(Chrome)" alt="">
+      </a>
+      <a target="_blank" rel="noopener noreferrer" href="https://addons.mozilla.org/en-US/firefox/addon/audible-library-extractor/">
+        <img src="https://img.shields.io/amo/v/audible-library-extractor?label=latest%20release%20(Firefox)" alt="">
+      </a>
+    </div>
+
+    <div class="current-version">
+      You're currently using version {{ $store.state.appVersion }}.
+    </div>
+  </div>
+</n-modal>
+</n-config-provider>
 </template>
- 
+
 <script>
+import { NConfigProvider, NModal, NTimeline, NTimelineItem, lightTheme } from 'naive-ui';
 import changelog from "@output-mixins/changelog.js";
 
 export default {
+  props: [ 'show' ],
+  emits: [ 'update:show' ],
   mixins: [ changelog ],
-  data: function() {
-    return {
-      
-    };
-  },
-  
-  methods: {
-    
-    getChangelog: function() {
-      
-      let changelogInnerHTML = '';
-      
-      _.each( this.changeLog, function( versionBlock ) {
-        
-        changelogInnerHTML += '<strong style="display: inline-block; width: 100%; font-size: 16px;">'+ versionBlock.version +'</strong>';
-        changelogInnerHTML += versionBlock.highlights ? '<div style="padding: 6px; margin: 6px 0 7px; color: rgb(128 93 54); background: rgb(246, 153, 50, .06); border: 1px solid rgb(246, 153, 50, .2);">'+ versionBlock.highlights +'</div>' : '';
-        changelogInnerHTML += '<ul class="ale-changelog-list" style="display: inline-block; width: 100%; box-sizing: border-box;">';
-        _.each( versionBlock.changes, function( change ) {
-          if ( change.divider ) {
-            changelogInnerHTML += '<li style="height: 0px; border: 1px dashed #f1f1f1; margin: 5px 0; width: 100%;"></li>';
-          }
-          else {
-            let linkText;
-            if ( change.link ) linkText = change.highlight ? ('<strong>' + change.link.text + '</strong>') : change.link.text;
-            changelogInnerHTML += '<li class="'+ (change.class || '') +'">'+
-              (change.link ? '<a target="_blank" rel="noopener noreferrer" href="'+ change.link.href +'">'+ linkText +'</a>: ' : '') + 
-              (change.description || '')
-            +'</li>';
-          }
-        });
-        changelogInnerHTML += '</ul>';
-        changelogInnerHTML += '<br><br>';
-      });
-      
-      let changelogHTML = '<div style="text-align: left; max-height: 350px; overflow: scroll; padding: 20px;">'+ changelogInnerHTML +'</div>';
-      return changelogHTML;
-      
-    },
-    
-  }
+  components: { NConfigProvider, NModal, NTimeline, NTimelineItem },
 }
 </script>
- 
-<style scoped src="@node/bulma/css/bulma.css"></style>
 
 <style lang="scss">
- 
+
 .ale-changelog-list {
   li { position: relative; z-index: 1; }
   li:before {
@@ -89,13 +92,56 @@ export default {
   li.removed:before    { background: #f25500; display: block; }
 }
 
-.changelog {
-  .icon {
-    font-size: 16px;
-    margin-right: 10px !important;
-    background: #f5f5f5;
-    padding: 4px;
-    border-radius: 20px;
+.changelog-divider {
+  height: 0;
+  border-top: 1px dashed #f1f1f1;
+  margin: 5px 0;
+}
+
+.version-highlights {
+  padding: 6px;
+  margin: 0 0 7px;
+  color: rgb(128 93 54);
+  background: rgb(246, 153, 50, .06);
+  border: 1px solid rgb(246, 153, 50, .2);
+  font-size: 0.85em;
+}
+
+.changelog-body {
+  text-align: left;
+  max-height: 50vh;
+  overflow: auto;
+  padding: 4px 4px 4px 0;
+}
+
+.project-info {
+  margin-top: 14px;
+  padding-top: 14px;
+  border-top: 1px solid #ededed;
+  font-size: 0.78em;
+  text-align: center;
+  color: #b5b5b5;
+
+  a {
+    color: #b5b5b5;
+    text-decoration: underline;
+    &:hover { color: #717171; }
+  }
+
+  .divider {
+    margin: 0 6px;
+  }
+
+  .project-badges {
+    margin-top: 10px;
+    opacity: 0.35;
+    transition: opacity 200ms ease;
+    img { display: inline-block; margin: 2px; }
+    &:hover { opacity: 1; }
+  }
+
+  .current-version {
+    margin-top: 8px;
   }
 }
 
